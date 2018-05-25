f69925313dda61564473b3ddff651a69e50b05fd
Ash <quarktheawesome@gmail.com>
2018-05-25 21:10:38 +1000

    powerpc/wiiu: Add espresso and latte PIC drivers

--- arch/powerpc/platforms/wiiu/wiiu.c
+++ arch/powerpc/platforms/wiiu/wiiu.c
@@ -3,12 +3,20 @@
 #include <asm/machdep.h>
 #include <asm/udbg.h>
 
+#include "drivers/espresso-pic.h"
+#include "drivers/latte-ahball-pic.h"
+
 static int __init wiiu_probe(void) {
     if (!of_machine_is_compatible("nintendo,wiiu")) {
         return 0;
     }
-    
+
     return 1;
+}
+
+static void wiiu_init_irq(void) {
+    espresso_pic_probe();
+    latte_ahball_pic_probe();
 }
 
 define_machine(wiiu) {
@@ -16,6 +24,8 @@ define_machine(wiiu) {
     .probe          = wiiu_probe,
     .progress       = udbg_progress,
     .calibrate_decr = generic_calibrate_decr,
+    .init_IRQ       = wiiu_init_irq,
+    .get_irq        = espresso_pic_get_irq,
 };
 
 static const struct of_device_id wiiu_of_bus[] = {
